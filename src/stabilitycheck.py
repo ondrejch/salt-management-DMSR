d420e222faa5d22c71ad8c8f445548f3d55ea59c
Gavin Ridley <gavin.keith.ridley@gmail.com>
2017-08-16 02:19:53 -0400

    started making stabilitycheck behave in a more serious way, incomplete

--- src/stabilitycheck.py
+++ src/stabilitycheck.py
@@ -41,7 +41,8 @@ def linear(x, m, b):
     """ the most complicated equation known to man."""
     return m*x + b
 
-def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
+def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1, doppler=None,
+                 geometry=None, voiding=None):
     """ Checks the stability of a SerpentInputFile object.
         The input file is re-written using the perl core writer.
         Reactivity is checked at 50 deg C increments away from 900.
@@ -50,10 +51,26 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
     Output:
         Float. Best fit temp coeff of reactivity"""
 
+    # temperatures to try, in K
+    # this is a linearization, really, so a point on either side
+    # of the nominal should work well
+    testT=[850.0, 950.0]
+
     # parameters for the analysis
     tmp_or_tms='tmp'
     perlcase=3 #case param for perl script. this one assumes graphite
                 # to be 50 K above the salt temperature
+
+    # string that describes which changes to neutronics domain are made
+    descripString = ''
+    if doppler:
+        descripString += 'Dopp'
+    if geometry:
+        descripString += 'Geom'
+    if voiding:
+        descripString += 'Void'
+    if descripString == '':
+        descripString = 'BaseCase'
 
     # copy core data for core writer
     coresize=inputfile.core_size
@@ -62,9 +79,27 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
     p=inputfile.pitch
     name = inputfile.inputfilename
 
+    # make a temporary input file just to obtain temperature-adjusted geometry:
+    # this only needs to be done once: future trial cases can include these geometry
+    # files
+    for thisTemp in testT:
+        tempInput = RefuelCore.SerpentInputFile(core_size=coresize,
+                                                salt_type=coresalt,
+                                                case=1,
+                                                salt_fraction=sf,
+                                                pitch=p,
+                                                initial_enrichment=0.01, #doesnt matter
+                                                num_nodes=999,
+                                                PPN=1234,
+                                                queue='herring' )
+        tempInput.SetInputFileName('tempAdjustGeom{}K'.format(thisTemp))
+        tempInput.WriteJob()
+        # now rename the resulting geometry file:
+        shutil.move('MSRs2_geom.inp', 'MSRs2_geom{}K.inp'.format(thisTemp))
+
     #construct new inputfiles
-    testT=[800.0,850.0,900.0, 950.0,1000.0,1100.0]
-    inplist=range(len(testT)) #initialize
+    inplist=range(len(testT)) # just a list with correct length
+                              # later will have inp file objs
 
     # each new input file must get a copy of the right fuel isotopics.
     fuelmat=None
@@ -76,7 +111,7 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
         raise Exception("fuel material not found in inputfile")
 
     for i,T in enumerate(testT):
-        dirname=name+'stability'+str(int(T))
+        dirname=name+'stability'+str(int(T)) + descripString
         if dirname in os.listdir('.'):
             os.system('rm -r {}'.format(dirname))
         os.mkdir(dirname)
@@ -86,7 +121,7 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
                                 initial_enrichment=0.0,num_nodes=nnodes,
                                 pitch=p,tempK=T,queue=queue, PPN=ppn)
         # change inputfile name
-        inplist[i].SetInputFileName(name+str(int(testT[i])))
+        inplist[i].SetInputFileName(name + str(int(testT[i])) + descripString)
 
         # need great MC resolution for this calculation
         inplist[i].ChangeKcodeSettings(int(5e4), 300, 40)
@@ -103,10 +138,18 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
         del inplist[i].materials[delindex]
         inplist[i].materials.append(copy.copy(fuelmat))
         # and now the fuel must have the correct test temperature
-        inplist[i].materials[-1].SetTemp(testT[i])
 
-        #annnddd submit!
+        # Write the jobs, making temperature changes ONLY after write 
+        # to ensure that ONLY changes we want are made (doppler, void, geom)
         inplist[i].WriteJob()
+        inplist[i].getMat("fuel").SetTemp(testT[i])
+
+        # adjust which geometry file is used as appropriate
+        if geometry:
+            inplist[i].includefiles.remove('MSRs2_geom.inp')
+            inplist[i].includefiles.append('MSRs2_geom{}K.inp'.format(testT[i]))
+
+
         inplist[i].SubmitJob()
 
         os.chdir('..')
@@ -125,7 +168,6 @@ def stabileCheck(inputfile, queue='gen5', ppn=8, verb=False, nnodes=1):
 
     #read reactivity
     rhos=[(inp.ReadKeff()-1.0)/inp.ReadKeff() for inp in inplist]
-
 
     #fit linear
     param=curve_fit(linear, testT, rhos)

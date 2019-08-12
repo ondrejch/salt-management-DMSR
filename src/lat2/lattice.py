35bb7017bfe117a22258992efff592ea80782d60
Ondrej Chvala <ondrejch@gmail.com>
2019-08-12 19:06:48 -0400

    prints deck

--- src/lat2/lattice.py
+++ src/lat2/lattice.py
@@ -18,32 +18,42 @@ SALTS = {
     "naf"    : "78%NaF + 22%UF4",
     "nafbe12": "74%NaF + 12%BeF2 + 14%UF4",
     "nafbe30": "58%NaF + 30%BeF2 + 12%UF4",
-    "nafkf"  : "50.5%NaF + 21.5%KF + 28%UF4",
-    "nafrbf2": "46%NaF + 33%RbF + 21%UF4" }
+    "nafrbf2": "46%NaF + 33%RbF + 21%UF4", 
+    "nafkf"  : "50.5%NaF + 21.5%KF + 28%UF4" }
 
-class Lat(object):
+NUCLEAR_DATA_LIBS = {'ENDF7', 'ENDF8', 'JEFF33' }
+
+do_plots = False
+
+class Lattice(object):
     'Infinite 2D lattice of graphite heaxagonal blocks with a cyllindrical fuel channel'
-    def __init__(self, salt:str, sf:float, l:float, e:float):
+    def __init__(self, salt:str='flibe', sf:float=0.1, l:float=20.0, e:float=0.015):
         try:                            # Check if salt is known
             self.salt_formula = SALTS[salt]
-        exept:
+        except:
             ValueError("Salt "+salt+" is undefined.")
-        if sf>0.6 or sf<0.0 or l<0.5 or l>2.0 or e>1.0 or e<0.0: # Reject bad input
-            exept ValueError("Bad parameters: ", sf, l, e)
+        if sf>0.6 or sf<1e-4 or l<0.05 or l>100.0 or e>1.0 or e<0.0: # Reject bad input
+            raise ValueError("Bad parameters: ", sf, l, e)
+
         # Lattice parameters
         self.l:float       = l          # Hex lattice size [cm]
         self.sf:float      = sf         # Fuel salt fraction
+        self.enr:float     = e          # Uranium enrichment
         self.r:float       = self.r()   # Dimater of the outer channel [cm]
-        sekf.enr:float     = enr        # Uranium enrichment
-        self.k:float       = -1.0       # ANALYTICAL_KEFF
-        self.kerr:float    = -1.0       # ANALYTICAL_KEFF Error
-        self.cr:float      = -1.0       # CONVERSION_RATIO
-        self.crerr:float   = -1.0       # CONVERSION_RATIO Error
-        self.salt:str      = "flibe"    # salt type
-        self.lib:str       = ""         # nuclear data library
+        self.salt_name:str = salt       # Salt identifier
+        self.s             = Salt(self.salt_formula, e) # Salt used
+        self.tempK:float   = 900.0      # Salt temperature [K]
+
+        self.k:float       = None       # ANALYTICAL_KEFF
+        self.kerr:float    = None       # ANALYTICAL_KEFF Error
+        self.cr:float      = None       # CONVERSION_RATIO
+        self.crerr:float   = None       # CONVERSION_RATIO Error
+
+        self.nuc_libs:str  = 'ENDF7'    # Nuclear data library
+        self.lib:str       = '09c'      # CE xsection temp selection
         self.queue:str     = 'fill'     # NEcluster torque queue
         self.ompcores:int  = 4          # OMP core count
-        self.deck_name:str = 'l.inp'    # Serpent input file name
+        self.deck_name:str = 'lat'      # Serpent input file name
         self.deck_path:str = '.'        # Where to run the lattice deck
         self.qsub_path:str = '.'        # Full path to qsub script
         self.main_path:str = '~/L'+salt # Main path
@@ -55,15 +65,15 @@ class Lat(object):
             "/%08.5f"%self.l + "/%012.9f"%self.enr
 #            "/%08.5f"%self.l 
 
-    def hexarea(self)->float:                  
+    def hexarea(self) -> float:                  
         'Area of the lattice [cm2]'
         return 2.0 * math.sqrt(3.0) * self.l**2
 
-    def r(self) ->float:
+    def r(self) -> float:
         'Radius of inner channel [cm]'
-        return math.sqrt(sf * l*l *math.sqrt(3.0) / (2.0 * math.pi))
+        return math.sqrt(self.sf * self.l**2 *math.sqrt(3.0) / (2.0 * math.pi))
 
-    def write_cells(self) ->str:
+    def get_cells(self) -> str:
         'Cell cards for Serpent input deck'
         cells = '''
 %______________cell definitions_____________________________________
@@ -71,21 +81,20 @@ cell 11  0  salt       -1      % fuel channel
 cell 12  0  graphite    1 -2   % graphite
 cell 99  0  outside     2      % graveyard
 '''
-        cells = cells.format(**locals())
-        return cells
+        return cells.format(**locals())
 
-    def write_surfaces(self) ->str:
+    def get_surfaces(self) -> str:
         'Surface cards for Serpent input deck'
         surfaces = '''
 %______________surface definitions__________________________________
 surf 1   cyl    0.0 0.0 {self.r}  % fuel channel radius
 surf 2   hexxc  0.0 0.0 {self.l}  % reflective unit cell boundary
 '''
-        surfaces = surfaces.format(**locals())
-        return surfaces
+        return surfaces.format(**locals())
 
-    def write_graphite(self) ->str:
+    def get_graphite(self) -> str:
         'Graphite material cards'
+        gr_lib   = self.lib
         gr_frac  = 1.0 - self.boron_graphite
         b10_frac = 0.2 * self.boron_graphite
         b11_frac = 0.8 * self.boron_graphite
@@ -94,18 +103,57 @@ surf 2   hexxc  0.0 0.0 {self.l}  % reflective unit cell boundary
 %  DENSITY: 1.82 G/CC
 mat graphite -1.82 moder graph 6000 tmp 973
 rgb 130 130 130
-6000.{lib} {gr_frac}
-5010.{lib} {b10_frac} % boron impirity eq.
-5011.{lib} {b11_frac}
+6000.{gr_lib} {gr_frac}
+5010.{gr_lib} {b10_frac} % boron impirity eq.
+5011.{gr_lib} {b11_frac}
 %  THERMAL SCATTERING LIBRARY FOR GRAPHITE
 therm graph gre7.08t
 '''
-        graphite = graphite.format(**locals())
-        return graphite
+        return graphite.format(**locals())
+
+    def get_data_cards(self) -> str:
+        'Data cards for the lattice'
+        data_cards = '''
+% Boundary condition
+set bc 3
+
+% Neutron population and criticality cycles
+set pop 10000 100 40 % 10000 neutrons, 100 active, 40 inactive cycles
+
+ Analog reaction rate
+% set arr 2
+'''     
+        if self.nuc_libs == "ENDF7":
+            data_cards += '''
+% Data Libraries
+set acelib "sss_endfb7u.sssdir"
+set declib "sss_endfb7.dec"
+set nfylib "sss_endfb7.nfy"
+'''
+        if do_plots:
+            data_cards += '''
+% Plots
+plot 3 1500 1500
+% mesh 3 1500 1500
+'''
+        return data_cards.format(**locals())
+
+    def get_deck(self) -> str:
+        'Serpent deck for the lattice'
+        deck = '''\
+set title "MSR lattice cell, l {self.l}, sf {self.sf}, salt {self.salt_formula}"
+'''     
+        deck += self.get_surfaces()
+        deck += self.get_cells()
+        deck += self.s.serpent_mat(self.tempK)
+        deck += self.get_graphite()
+        deck += self.get_data_cards()
+
+        return deck.format(**locals())
 
     def save_deck(self):
         'Saves Serpent deck into an input file'
-        s2_deck = self.serpent_deck()
+        s2_deck = self.get_deck()
         try:
             fh = open(self.deck_path + '/' + self.deck_name, 'w')
             fh.write(s2_deck)
@@ -114,7 +162,7 @@ therm graph gre7.08t
             print("[ERROR] Unable to write to file: ", self.deck_name)
             print(e)
 
-    def save_qsub_file(self)->bool:
+    def save_qsub_file(self) -> bool:
         'Writes run file for TORQUE.'
         qsub_content ='''#!/bin/bash
 #PBS -V
@@ -146,11 +194,11 @@ awk 'BEGIN{{ORS="\\t"}} /ANA_KEFF/ || /CONVERSION/ {{print $7" "$8;}}' {filename
 
 # -------------------------------------------------------------
 
-def hexarea(l:float) ->float:
+def hexarea(l:float) -> float:
     'Area of the lattice [cm2]'
     return 2.0 * math.sqrt(3.0) * l**2
 
-def r(sf:float, l:float) ->float:
+def r(sf:float, l:float) -> float:
     'Radius of inner channel [cm]'
 # Area of an equilateral triangle:      Sspark = d^2 * sqrt(3)/4
 # Angle is 60deg => area of one 60deg vedge:    Svedge = 1/6 * pi * r^2
@@ -163,6 +211,7 @@ def r(sf:float, l:float) ->float:
 
 if __name__ == '__main__':
     print("This module handles a simple lattice.")
-    input("Press Ctrl+C to quit, or enter else to test it.")
-    print("TODO")
+#    input("Press Ctrl+C to quit, or enter else to test it.")
+    l = Lattice()
+    print(l.get_deck())
 
